46d04a72d32c48bfee7eb3eddb806df4b685869c
Justin <Jefferson@zipcoders-MacBook-Pro-10.local>
2019-05-22 11:01:47 -0400

    Finished Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner userIn = new Scanner(System.in);
+        
+        System.out.print("What is your name? ");
+        String name = userIn.next();
+        
+        if((name.equalsIgnoreCase("Alice")) || (name.equalsIgnoreCase("Bob"))) {
+            System.out.println("Greetings " + name + ". Nice to meet you.");
+        }
+        else {
+            System.out.println("I'm sorry. I don't recognize that user. TERMINATE!");
+        }
 
     }
 }
